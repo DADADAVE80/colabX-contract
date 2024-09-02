f19f6e8da739c43ff5ed5a761d1a6afcad95c84b
dadadave80 <daveproxy80@gmail.com>
2024-09-02 23:45:26 +0100

    feat: install ts-node pkg and setup dABI

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
+import "hardhat-diamond-abi";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.25",
+  diamondAbi: {
+    // (required) The name of your Diamond ABI
+    name: "ColabX-dApp",
+    // filter: function (abiElement, index, fullAbi, fullyQualifiedName) {
+    //   return abiElement.name !== "superSecret";
+    // },
+    // Whether exact duplicate sighashes should cause an error to be thrown,
+    // defaults to true.
+    strict: false,
+  },
 };
 
 export default config;
